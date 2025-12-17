4c5b6bad0686115471a0f027e5834e7ad8077a10
chiranat <chiranat.pae@gmail.com>
2025-12-17 21:38:44 +0700

    fix cros

--- backend/src/main/java/com/chiranat/pos/controller/FileUploadController.java
+++ backend/src/main/java/com/chiranat/pos/controller/FileUploadController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/admin/upload")
 @RequiredArgsConstructor
